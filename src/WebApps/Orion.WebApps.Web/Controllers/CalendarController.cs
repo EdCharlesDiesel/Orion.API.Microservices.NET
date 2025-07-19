1413394ce5528e933b094f7d7fc857ea52def6c3
unknown <Mokhetkc@hotmail.com>
2025-07-19 21:27:55 +0200

    ,

--- src/WebApps/Orion.WebApps.Web/Controllers/CalendarController.cs
+++ src/WebApps/Orion.WebApps.Web/Controllers/CalendarController.cs
@@ -1,115 +1,119 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Orion.StockAnalyzer.Core.Domain;
+using Orion.WebApps.Web.Helper;
 
 namespace Orion.WebApps.Web.Controllers;
 
 public class CalendarController : Controller
 {
     private readonly IHttpClientFactory _httpClientFactory;
+    private readonly ApiSettings _apiSettings;
 
-    public CalendarController(IHttpClientFactory httpClientFactory)
+    public CalendarController(IHttpClientFactory httpClientFactory, IOptions<ApiSettings> apiOptions)
     {
         _httpClientFactory = httpClientFactory;
+        _apiSettings = apiOptions.Value;
     }
     // GET: /Calendar
     public async Task<IActionResult> Index()
     {
         var client = _httpClientFactory.CreateClient();
-        var response = await client.GetAsync("https://api.example.com/data");
 
-        if (response.IsSuccessStatusCode)
+        try
         {
-            var result = await response.Content.ReadAsStringAsync();
-            ViewBag.ApiResult = result;
-            return View();
+            var events = await client.GetFromJsonAsync<List<CalendarEvent>>(_apiSettings.CalendarApiUrl);
+            return View(events);
         }
-
-        ViewBag.ApiResult = "Error: " + response.StatusCode;
-        return View();
+        catch (HttpRequestException ex)
+        {
+            ViewBag.Error = "API error: " + ex.Message;
+            return View("Error");
+        }
     }
 
-        // GET: /Calendar/Details/{id}
-        public IActionResult Details(Guid id)
-        {
-            var client = _httpClientFactory.CreateClient();
-            var response = await client.GetAsync("https://api.example.com/data");
-            var item = _calendarEvents.FirstOrDefault(c => c.Id == id);
-            if (item == null)
-                return NotFound();
-
-            return View(item);
-        }
-
-        // GET: /Calendar/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: /Calendar/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Calendar calendar)
-        {
-            if (ModelState.IsValid)
-            {
-                calendar.Id = Guid.NewGuid();
-                _calendarEvents.Add(calendar);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(calendar);
-        }
-
-        // GET: /Calendar/Edit/{id}
-        public IActionResult Edit(Guid id)
-        {
-            var item = _calendarEvents.FirstOrDefault(c => c.Id == id);
-            if (item == null)
-                return NotFound();
-
-            return View(item);
-        }
-
-        // POST: /Calendar/Edit/{id}
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(Guid id, Calendar updated)
-        {
-            var item = _calendarEvents.FirstOrDefault(c => c.Id == id);
-            if (item == null)
-                return NotFound();
-
-            if (ModelState.IsValid)
-            {
-                item.EventName = updated.EventName;
-                item.Date = updated.Date;
-                return RedirectToAction(nameof(Index));
-            }
-
-            return View(updated);
-        }
-
-        // GET: /Calendar/Delete/{id}
-        public IActionResult Delete(Guid id)
-        {
-            var item = _calendarEvents.FirstOrDefault(c => c.Id == id);
-            if (item == null)
-                return NotFound();
-
-            return View(item);
-        }
-
-        // POST: /Calendar/Delete/{id}
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(Guid id)
-        {
-            var item = _calendarEvents.FirstOrDefault(c => c.Id == id);
-            if (item != null)
-            {
-                _calendarEvents.Remove(item);
-            }
-            return RedirectToAction(nameof(Index));
-        }
+        // // GET: /Calendar/Details/{id}
+        // public IActionResult Details(Guid id)
+        // {
+        //     var client = _httpClientFactory.CreateClient();
+        //     var response = await client.GetAsync("https://api.example.com/data");
+        //     var item = _calendarEvents.FirstOrDefault(c => c.Id == id);
+        //     if (item == null)
+        //         return NotFound();
+        //
+        //     return View(item);
+        // }
+        //
+        // // GET: /Calendar/Create
+        // public IActionResult Create()
+        // {
+        //     return View();
+        // }
+        //
+        // // POST: /Calendar/Create
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public IActionResult Create(Calendar calendar)
+        // {
+        //     if (ModelState.IsValid)
+        //     {
+        //         calendar.Id = Guid.NewGuid();
+        //         _calendarEvents.Add(calendar);
+        //         return RedirectToAction(nameof(Index));
+        //     }
+        //     return View(calendar);
+        // }
+        //
+        // // GET: /Calendar/Edit/{id}
+        // public IActionResult Edit(Guid id)
+        // {
+        //     var item = _calendarEvents.FirstOrDefault(c => c.Id == id);
+        //     if (item == null)
+        //         return NotFound();
+        //
+        //     return View(item);
+        // }
+        //
+        // // POST: /Calendar/Edit/{id}
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public IActionResult Edit(Guid id, Calendar updated)
+        // {
+        //     var item = _calendarEvents.FirstOrDefault(c => c.Id == id);
+        //     if (item == null)
+        //         return NotFound();
+        //
+        //     if (ModelState.IsValid)
+        //     {
+        //         item.EventName = updated.EventName;
+        //         item.Date = updated.Date;
+        //         return RedirectToAction(nameof(Index));
+        //     }
+        //
+        //     return View(updated);
+        // }
+        //
+        // // GET: /Calendar/Delete/{id}
+        // public IActionResult Delete(Guid id)
+        // {
+        //     var item = _calendarEvents.FirstOrDefault(c => c.Id == id);
+        //     if (item == null)
+        //         return NotFound();
+        //
+        //     return View(item);
+        // }
+        //
+        // // POST: /Calendar/Delete/{id}
+        // [HttpPost, ActionName("Delete")]
+        // [ValidateAntiForgeryToken]
+        // public IActionResult DeleteConfirmed(Guid id)
+        // {
+        //     var item = _calendarEvents.FirstOrDefault(c => c.Id == id);
+        //     if (item != null)
+        //     {
+        //         _calendarEvents.Remove(item);
+        //     }
+        //     return RedirectToAction(nameof(Index));
+        // }
 }
---
+++ src/WebApps/Orion.WebApps.Web/Helper/ApiSettings.cs
@@ -0,0 +1,7 @@
+namespace Orion.WebApps.Web.Helper;
+
+
+    public class ApiSettings
+    {
+        public string CalendarApiUrl { get; set; }
+    }
