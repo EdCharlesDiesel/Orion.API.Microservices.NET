981a88b624af0a54f57bf641556cc0ae2926aeab
unknown <Mokhetkc@hotmail.com>
2025-07-19 21:27:55 +0200

    ,

--- src/WebApps/Orion.WebApps.Web/obj/Debug/net9.0/Orion.WebApps.Web.AssemblyInfo.cs
+++ src/WebApps/Orion.WebApps.Web/obj/Debug/net9.0/Orion.WebApps.Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orion.WebApps.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+228466533adc53e985660967181af4dfa5a9872e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fccc2f588daf00f438f9da82c39f064cc27a7e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orion.WebApps.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orion.WebApps.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
