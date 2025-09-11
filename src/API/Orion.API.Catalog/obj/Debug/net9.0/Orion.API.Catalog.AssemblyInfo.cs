b57108d283b2f5ed1f6c2287fb215f17add14400
BATMAN <Mokhetkc@hotmail.com>
2025-09-11 23:26:48 +0200

    Error log
    Error log

--- src/API/Orion.API.Catalog/obj/Debug/net9.0/Orion.API.Catalog.AssemblyInfo.cs
+++ src/API/Orion.API.Catalog/obj/Debug/net9.0/Orion.API.Catalog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orion.API.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dd14ba30db13a7fb0f9e31f8ab2597ae85a5bd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bb8731c707c7b58c0a7ac6ebd2dbcd891bd1b63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orion.API.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orion.API.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
